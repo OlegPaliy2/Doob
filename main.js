c03472024dec934e713805966e71bb5e88935d78
OlegPaliy <olegpaliy1986@gmail.com>
2022-01-03 22:03:09 +0200

    mobile ready

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-
-(function(){
+(function () {
     let button = document.querySelector(".burger");
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', () => {
         button.classList.toggle('burgerClose')
     })
 })();
+
+(function () {
+    let seeMore = document.querySelector(".seeMore");
+    let workHide = document.querySelectorAll(".workHide");
+    seeMore.addEventListener('click', () => {
+        seeMore.classList.toggle('seeLess');
+        workHide.forEach(w => w.classList.toggle('work'));
+    })
+})();
+
+$('.seeMore').click(function(){
+    const id = $(this).attr('href');
+    const offset = $(id).offset().top;
+    $('html').animate({
+        scrollTop: offset
+    }, 500)
+})
